4bf27d916dce3b4b1c1b9b4fda37afdd5fd475ea
john mokaya <mokayaj857@gmail.com>
2025-01-06 20:00:49 +0300

    Update python assignment.py

--- python assignment.py
+++ python assignment.py
@@ -6,6 +6,6 @@ age = input("Please enter your age: ")
 
 # Ask for the user's location
 location = input("Please enter your location: ")
-
+dfcv
 # Print out a personalized message
 print(f"Hello {name}, you are {age} years old and live in {location}.")
